051457820ca772172006eb57efe8f6701a0cca18
RayanAlkhelaiwi <r.n.m.k@live.com>
2020-05-14 11:02:58 +0300

    Add Todo Lists to store todos inside
    With one-to-many relationship, obviously :)

--- app.py
+++ app.py
@@ -15,7 +15,14 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
     # Returns a printable statement showing the results of the table (For testing)
     # def __repr__(self):
     #     return f'<Todo {self.id}: {self.description}>'
